9a46c3c7e34475482058e85875d3ef1f38aa55be
Luciano Renda <108601802+lsrenda@users.noreply.github.com>
2026-09-23 00:36:59 -0300

    Add comment for testing pull requests
    Added a comment for testing pull requests.

--- main.c
+++ main.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h> // Necesario para las funciones de asignación de memoria dinámica: malloc y free
-
+// Agregado un comentario desde GitHub para probar pull
 int main() {
     int *arr; // Declaramos un puntero a un entero, que apuntará al inicio de nuestro arreglo dinámico
     int n;    // Variable para almacenar el tamaño deseado del arreglo
